8a13a798cdf90ec1982d464ab5c5e91de3c1aa0a
Romero Meireles <romero.mfm@gmail.com>
2015-09-23 10:08:59 -0300

    BooksController, crud methods, book validation_presence_of, routes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+	before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+	def index
+      @books = Book.all
+	end
+
+	def new
+      @book = Book.new
+	end
+
+	def show
+	end	
+
+    def create      
+      if @book.save
+        redirect_to @book, notice: "Book created successfully"
+      else
+      	render action: :new, notice: "Could not update this book"
+      end
+	end
+
+	def edit       
+	end
+
+	def update
+	  if @book.update(book_params)
+	  	redirect_to @book, notice: "Book updated successfully"
+	  else
+        redirect_to @book, notice: "Could not update this book"
+	  end
+	end
+
+	def destroy
+      if @book.destroy
+	    redirect_to :books_path, notice: "Book removed successfully"
+	  else
+	  	redirect_to :books_path, notice: "Book could not be removed"
+	  end
+	end
+
+    ##### helper methods ######
+    private
+
+	def book_params
+      params.require(:book).
+        permit(:isbn, :title, :author, :spiritual_author, :publisher, :cost, :price, :year, :pages_number)
+	end
+
+	def set_book
+	  @book = Book.find(params[:id])
+	end
+end
