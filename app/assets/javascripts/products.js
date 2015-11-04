f948a7242b5135840010431d799c4132594579a8
Romero Meireles <romero.mfm@gmail.com>
2015-11-04 15:27:40 -0300

    Dynamic path for New product button

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function set_path_for_new_btn() {
+  alert('kkkk');
+  var sel_val = $('#product').val();
+  $('#product_link_to').attr('href', sel_val)
+}
