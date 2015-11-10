0ff30e8c7972c03185fe23938b9cfdb15fb5e87e
Romero Meireles <romero.mfm@gmail.com>
2015-11-10 15:22:43 -0300

    Login page style

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,8 @@
 class Sale < ActiveRecord::Base
   has_many :sale_items, dependent: :destroy
   belongs_to :user
+
+  def subtotal
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
 end
