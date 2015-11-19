73deb88c1e092d75833188700af40755d43c81fd
Romero Meireles <romero.mfm@gmail.com>
2015-11-19 09:31:41 -0300

    Update SaleItem quantity

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -6,11 +6,19 @@ class SaleItemsController < ApplicationController
   def create    
     @sale = current_sale
     @sale_item = @sale.sale_items.new(sale_item_params)
-    @sale.user = current_user
-    @sale.buyer_name = current_user.name
-    @sale.total = @sale.total_price
-    @sale.save
-    session[:sale_id] = @sale.id
+
+    if @sale.id.present?
+      sale_items = SaleItem.joins(:sale).where({sale_id: @sale.id, product_id: @sale_item.product_id, 'sales.sale_status_id' => 1})
+      @sale_item = sale_items[0]
+      new_product_quantity = @sale_item.product_quantity.to_i + sale_item_params[:product_quantity].to_i
+      @sale_item.update(product_quantity: new_product_quantity)
+    else
+      @sale.user = current_user
+      @sale.buyer_name = current_user.name
+      @sale.total = @sale.total_price
+      @sale.save
+      session[:sale_id] = @sale.id
+    end   
   end
 
   def update
