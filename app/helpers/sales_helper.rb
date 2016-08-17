246f3458b35b112aa8921f90089a68a770dd1bf5
Victor Ximenis <victorximenis@gmail.com>
2016-08-17 17:49:27 -0300

    Sales history

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,9 +1,20 @@
 module SalesHelper
-  def cancel_sale_before_end_session    
+  def cancel_sale_before_end_session
     if session[:sale_id].present?
       sale = Sale.find(session[:sale_id])
       sale.update(status: :canceled)
       current_sale = nil
     end
+  end
+  def items_to_list(items)
+    item_list = ""
+    items.each do |item|
+      begin
+        item_list += item.product.title
+      rescue
+        item_list += "Indefinido".html_safe
+      end
+    end
+    return item_list
   end
 end
