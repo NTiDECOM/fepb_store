afc915dfd6481d9ac70db02e7ff822ea855853ad
Romero Meireles <romero.mfm@gmail.com>
2016-09-19 02:51:47 -0300

    Fixing ticket update error when trying to sabe with a null date

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,10 @@
 class Ticket < Product
   def formatted_event_datetime
-    self[:event_datetime].strftime("%d/%m/%Y - %H:%M:%S") if !self[:event_datetime].nil?
+    if self[:event_datetime].nil?
+      self[:event_datetime] = DateTime.now + 6.months
+      self[:event_datetime].strftime("%d/%m/%Y - %H:%M:%S")
+    else 
+      self[:event_datetime].strftime("%d/%m/%Y - %H:%M:%S")
+    end
   end
 end
